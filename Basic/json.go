e53ee9216b9a7fd5bf887019557a874a623ff432
xiemenger <tbaby.menger@gmail.com>
2018-11-29 22:44:40 -0500

    json to map, struct

--- Basic/json.go
+++ Basic/json.go
@@ -44,4 +44,38 @@ func main() {
 	fmt.Println("result = ", string(res))
 
 	// ------用json存到结构体 --------
+	jsonBuf := `
+	{
+		"company" : "itcast",
+		"subjects":[
+			"Go",
+			"C++",
+			"Python",
+			"Test"
+		],
+		"isok" : true,
+		"price": 666.666
+	}`
+
+	m2 := make(map[string]interface{}, 4)
+	err = json.Unmarshal([]byte(jsonBuf), &m2)
+	if err != nil {
+		fmt.Println("err = ", err)
+		return
+	}
+	fmt.Printf("m = %+v\n", m2)
+
+	// 下面两行是不work的, 因为m["company"]是interface{}类型,
+	// 不能强转为string类型
+	// var str string
+	// str = m["company"]
+
+	//用类型断言
+	for k, v := range m {
+		switch value := v.(type) {
+		case string:
+			fmt.Printf("map[%s] ==> type is string, v = %s\n", k, value)
+
+		}
+	}
 }
